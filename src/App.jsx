314a91c0f01d3505fedeeef41b18e691cc54e44c
jaidm <jaidmujawar018@gmail.com>
2025-06-22 15:52:29 +0530

    jaid first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import CoursesPage from "./pages/CoursesPage";
+import ContactPage from "./pages/ContactPage";
+import AdmissionPage from "./pages/AdmissionPage";
+import './styles/Pages.css'
+import ChatbotComponent from "./components/Chatbot/ChatbotComponents";
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/Home" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/courses" element={<CoursesPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/admission" element={<AdmissionPage />} />
+        </Routes>
+      </Router>
+      <div>
+        <ChatbotComponent />
+      </div>
+    </div>
+  );
+};
+
+export default App;
+ 
